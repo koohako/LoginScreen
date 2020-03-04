a7fb5e984132ea66bdd84f4047ba20d61167699d
koohako <arturguimaraesdefreitas@gmail.com>
2020-03-03 22:00:18 -0300

    ajustes tela principal

--- app/src/main/java/com/Metatech/PrincipalActivity.java
+++ app/src/main/java/com/Metatech/PrincipalActivity.java
@@ -12,15 +12,6 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        //ao criar a tela principal
-        //pegar o e-mail e senha digitado
-        Intent i = getIntent();
-        String email = i.getStringExtra(MainActivity.EXTRA_MENSSAGE_EMAIL);
-        String senha = i.getStringExtra(MainActivity.EXTRA_MENSSAGE_SENHA);
 
-        //inserir valor nos EditText
-        EditText editEmail = findViewById(R.id.editEmail);
-        editEmail.setText(email);
-        //a senha não sera visualizada pelo usuario
     }
 }
